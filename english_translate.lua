fa402a3173664d261f4bb943d1746aaab5a9500a
Rafał Krzyważnia <r.krzywaznia@gmail.com>
2023-03-12 22:13:10 +0100

    update

--- english_translate.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-M.name = "translate to english"
-
-M.prompt = [[you are an american native speaker, translate text below into English and improve grammar.
-             use simple and common vocabulery. don't use complicated gramaar and tenses.
-             $1]]
-
-return M
